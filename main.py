c624238cc66eef933b59cb4b2ea0bd734b740424
tenley22 <89983912+tenley22@users.noreply.github.com>
2021-10-15 08:35:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ BLUE = (7, 71, 92)
 GREEN = (6, 69, 22)
 GREEN2 = (22, 87, 0)
 WHITE = (255, 255, 255)
-YELLOW = (255, 220, 71)
+YELLOW = (255, 221, 71)
 STARS = [WHITE, YELLOW]
 
 # math constants
@@ -27,10 +27,11 @@ screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption('Forest Scene')
 
 star_x = 50
-starx_velo = 1
+starx_velo = 2
 star_y = 50
-stary_velo = 1
-star_rad = 8
+stary_velo = 2
+star_side = 40
+
 
 running = True
 while running:
@@ -38,47 +39,47 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+    if star_x + star_side > DISPLAY_WIDTH or star_x < 0:
+        starx_velo *= -1
 
-    pygame.display.flip()
+    if star_y + star_side > 200 or star_y < 0:
+        stary_velo *= -1
+  
+
+
 
     screen.fill(BLUE)
 
-    # functions
-    def trees(place, fill, x1, y1, mult):
-        space = 180 * mult
-        pygame.draw.polygon(place, fill, ((x1 + space, y1), (x1 - 50 + space, y1 + 150), (x1 - 25 + space, y1 + 130), (x1 - 60 + space, y1 + 250),
-        (x1 - 35 + space, y1 + 230), (x1 - 100 + space, 700), (x1 + 100 + space, 700), (x1 + 25 + space, y1 + 230),
-        (x1 + 50 + space, y1 + 250), (x1 + 15 + space, y1 + 130), (x1 + 40 + space, y1 + 150)))
-
-    def stars(place, fill, x1, y1, width, mult = 0):
-        space = 75 * mult
-        pygame.draw.circle(place, fill, (x1 + space, y1), width)
-
-
     # drawing
-
-    stars(screen, WHITE, star_x, star_y, star_rad)
+    #star
+    
+    color = random.choice(STARS)
+    pygame.draw.polygon(screen, color, ((star_x, star_y), (star_x-5, star_y-20), (star_x-30, star_y-30), (star_x-5, star_y-40), (star_x, star_y-70), (star_x+5, star_y-40), (star_x+30, star_y-30), (star_x+5, star_y-20)))
     star_x += starx_velo
     star_y += stary_velo
-    if star_x > DISPLAY_WIDTH or star_x < 0:
-        starx_velo *= -1
-
-    if star_y > DISPLAY_HEIGHT or star_y < 0:
-        stary_velo *= -1
+  
 
 
-    # stars
+    # stars original 
+    '''
     for multiplier in range(20):
         color = random.choice(STARS)
-        stars(screen, color, -10, 130, 8, multiplier)
-        stars(screen, color, 40, 70, 8, multiplier)
-        stars(screen, color, 20, 10, 8, multiplier)
-
+        num = random.randint(10, 20)
+        pygame.draw.circle(screen, color, (-10 + 75 * multiplier, 130), 8)
+        pygame.draw.circle(screen, color, (40 + 75 * multiplier, 70), 8)
+        pygame.draw.circle(screen, color, (20 + 75 * multiplier, 10), 8)
+    '''
 
     # trees
     for multiplier in range (4):
-        trees(screen, GREEN, 100, 100, multiplier)
-        trees(screen, GREEN2, 20, 100, multiplier)
+        pygame.draw.polygon(screen, GREEN, ((100 + 180*multiplier, 100), (50 + 180*multiplier, 250), (75 + 180*multiplier, 230), (40 + 180*multiplier, 350), (65 + 180*multiplier, 330), (0 + 180*multiplier, 700), (200 + 180*multiplier, 700), (125 + 180*multiplier, 330), (150 + 180*multiplier, 350), (115 + 180*multiplier, 230), (140 + 180*multiplier, 250)))
 
+        pygame.draw.polygon(screen, GREEN2, ((20 + 180 * multiplier, 100), (-40 + 180 * multiplier, 250), (-15 + 180 * multiplier, 230), (-50 + 180 * multiplier, 350), (-25 + 180 * multiplier, 330), (-80 + 180 * multiplier, 700), (120 + 180 * multiplier, 700), (35 + 180 * multiplier, 330), (60 + 180 * multiplier, 350), (25 + 180 * multiplier, 230), (50 + 180 * multiplier, 250)))
+
+
+
+
+    pygame.display.flip()
 
 pygame.quit()
